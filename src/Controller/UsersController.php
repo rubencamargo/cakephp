dfaa9123ca7d15a5f7ebce34d6185419e31fec11
Ruben Camargo <rubencamargo@gmail.com>
2021-09-18 16:07:13 -0300

    Fix logout redirect

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -50,7 +50,7 @@ class UsersController extends AppController
         // regardless of POST or GET, redirect if user is logged in
         if ($result->isValid()) {
             $this->Authentication->logout();
-            return $this->redirect(['controller' => 'Users', 'action' => 'login']);
+            return $this->redirect(['controller' => 'Articles', 'action' => 'blog']);
         }
     }
     
