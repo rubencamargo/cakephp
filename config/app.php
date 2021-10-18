bd44fb46debbb873390965dbb837b18617f4f7c9
Ruben Camargo <rubencamargo@gmail.com>
2021-10-18 12:00:37 -0300

    Database sessions.

--- config/app.php
+++ config/app.php
@@ -416,6 +416,6 @@ return [
      * To use database sessions, load the SQL file located at config/schema/sessions.sql
      */
     'Session' => [
-        'defaults' => 'php',
+        'defaults' => 'database',
     ],
 ];
