36067916d194b797f0f3e912c9af39f41c095799
Ruben Camargo <rubencamargo@gmail.com>
2021-10-28 11:18:26 -0300

    Errors views

--- templates/Error/error500.php
+++ templates/Error/error500.php
@@ -8,38 +8,49 @@
 use Cake\Core\Configure;
 use Cake\Error\Debugger;
 
-$this->layout = 'error';
+//$this->layout = 'error';
+
+echo $this->Html->image('error.jpg');
 
 if (Configure::read('debug')) :
-    $this->layout = 'dev_error';
+    //$this->layout = 'dev_error';
 
     $this->assign('title', $message);
     $this->assign('templateName', 'error500.php');
 
     $this->start('file');
 ?>
+
 <?php if (!empty($error->queryString)) : ?>
     <p class="notice">
         <strong>SQL Query: </strong>
         <?= h($error->queryString) ?>
     </p>
 <?php endif; ?>
+
 <?php if (!empty($error->params)) : ?>
     <strong>SQL Query Params: </strong>
     <?php Debugger::dump($error->params) ?>
 <?php endif; ?>
+
 <?php if ($error instanceof Error) : ?>
     <strong>Error in: </strong>
     <?= sprintf('%s, line %s', str_replace(ROOT, 'ROOT', $error->getFile()), $error->getLine()) ?>
 <?php endif; ?>
+
 <?php
     echo $this->element('auto_table_warning');
 
     $this->end();
 endif;
 ?>
-<h2><?= __d('cake', 'An Internal Error Has Occurred.') ?></h2>
+
+<h2><?= __('An error has occurred.') ?></h2>
 <p class="error">
     <strong><?= __d('cake', 'Error') ?>: </strong>
     <?= h($message) ?>
 </p>
+
+<br />
+
+<a href="<?= $this->Url->build('/') ?>"><?= __('Back') ?></a>
