80f40ba7a2a608448dce3ef2994691fa3d7d93d5
Ruben Camargo <rubencamargo@gmail.com>
2021-11-16 16:31:33 -0300

    Composer update

--- vendor/composer/composer/src/Composer/Composer.php
+++ vendor/composer/composer/src/Composer/Composer.php
@@ -51,9 +51,9 @@ class Composer
      * const RELEASE_DATE = '@release_date@';
      * const SOURCE_VERSION = '1.8-dev+source';
      */
-    const VERSION = '2.1.11';
+    const VERSION = '2.1.12';
     const BRANCH_ALIAS_VERSION = '';
-    const RELEASE_DATE = '2021-11-02 12:10:25';
+    const RELEASE_DATE = '2021-11-09 16:02:04';
     const SOURCE_VERSION = '';
 
     /**
