b4c438844d8126312930d32c53a85169bdf06bc5
Ruben Camargo <rubencamargo@gmail.com>
2021-11-16 16:39:40 -0300

    CRUD buttons styles

---
+++ webroot/js/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$("a:contains('Ver')").addClass('button success');
+	$("a:contains('Modificar')").addClass('button warning');
+	$("a:contains('Eliminar')").addClass('button danger');
+});
